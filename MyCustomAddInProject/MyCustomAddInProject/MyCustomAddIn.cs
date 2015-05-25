f3c37300dddaf15eab1ff4afe99134040cb96de6
daxnet <daxnet@outlook.com>
2015-05-25 16:05:28 +0800

    Initial create

---
+++ MyCustomAddInProject/MyCustomAddInProject/MyCustomAddIn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spotfire.Dxp.Application.Extension;
+
+namespace MyCustomAddInProject
+{
+    public class MyCustomAddIn : AddIn
+    {
+
+    }
+}
